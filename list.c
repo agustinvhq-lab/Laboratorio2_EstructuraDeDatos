19445b1a07a907cd762329dc8f9838b5c98ff845
Your Name <you@example.com>
2026-03-28 01:25:21 +0000

    new try

--- list.c
+++ list.c
@@ -60,11 +60,13 @@ void * nextList(List * list) {
 //   - La segunda función retorna el dato del nodo anterior a current y actualiza el current para que apunte a ese nodo.
 
 void * lastList(List * list) {
+    if (list->tail == NULL) return NULL;
     list->current = list->tail;
     return list->current->data;
 }
 
 void * prevList(List * list) {
+    if (list->current->prev == NULL) return NULL;
     list->current = list->current->prev;
     return list->current->data;
 }
